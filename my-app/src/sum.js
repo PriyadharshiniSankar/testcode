7eebe7d3e2958c51a8ab8306f8085873b35b818a
PriyadharshiniSankar <sankarleela2428@gmail.com>
2022-05-08 23:53:44 -0700

    first commit

---
+++ my-app/src/sum.js
@@ -0,0 +1,11 @@
+function sum(x, y){
+    // add two numbers and return result
+return x + y;
+}
+
+// call function
+let n = sum(10, 18);
+// display the output
+console.log(n);
+
+//end of program
